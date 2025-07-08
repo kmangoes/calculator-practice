f53672c155bd5cb251ec73b56a3a5d2771d35b2b
Chase <sp24lights@gmail.com>
2025-07-07 22:51:55 -0400

    keyboard input update

--- App.js
+++ App.js
@@ -14,3 +14,18 @@ function calculate() {
         display.value = "Error"; 
     }
 }
+function backspace(){
+    display.value = display.value.slice(0,-1); 
+}
+document.addEventListener('keydown', function(event){
+    const key = event.key;
+    if(!isNaN(key)){
+        appendToDisplay(key); 
+    } else if(['+','-','*','/','.'].includes(key)){
+        appendToDisplay(key); 
+    } else if (key === 'Enter') {
+        calculate(); 
+    } else if (key === 'C' || key === 'Escape') {
+        clearDisplay(); 
+    } 
+}); 
